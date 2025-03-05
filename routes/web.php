7a743231e86b528fcd5bb857c49594c2caeb7275
DrecQ <drecquist@gmail.com>
2025-03-05 12:55:49 +0100

    Creation du model [property] / Implementation des routes avec resources / Connexion à MySQL

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Models\Property;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+//Creation de la route de gestion de la partie administrator
+
+Route::prefix('admin')->name('admin')->group(function() {
+   
+    Route::resource('property', \App\Http\Controllers\Admin\PropertyController::class);
+});
