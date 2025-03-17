84d8ceb732e56d69b08f6418545b81a970975190
DrecQ <drecquist@gmail.com>
2025-03-17 17:38:15 +0100

    Introduction et explication du casting / Modification et personnalisation des formats de données de Laravel

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Property;
+use Illuminate\Foundation\Auth\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -12,6 +13,11 @@ class HomeController extends Controller
     public function index()
     {
         $properties = Property::orderBy('created_at', 'DESC')->available()->recent()->limit(6)->get();
+
+        $user = User::first();
+        $user -> password = '1111';
+
+    
 
         return view('home', ['properties' => $properties]);
     }
