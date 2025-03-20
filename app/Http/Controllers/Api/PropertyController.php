3019b1e3b08beda68a9aaa7563527808f3117f28
DrecQ <drecquist@gmail.com>
2025-03-20 11:54:14 +0100

    Manipulation des collections pour les Api Resource

--- app/Http/Controllers/Api/PropertyController.php
+++ app/Http/Controllers/Api/PropertyController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\PropertyCollection;
 use App\Http\Resources\PropertyResource;
 use App\Models\Property;
 use Illuminate\Http\Request;
@@ -12,8 +13,8 @@ class PropertyController extends Controller
     //
     public function index()
     {
-        return PropertyResource::collection(Property::Paginate(5));
-        return PropertyResource::collection(Property::limit(5)->with('options')->get());
+        // return PropertyResource::collection(Property::Paginate(5));
+        return new PropertyCollection(Property::limit(5)->with('options')->get());
         return new PropertyResource(Property::find(1));
 
     }
